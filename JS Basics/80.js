d50e5f263463c1804c2be67140917ea94f6d311b
Maksim Dorogoi <monarch2013@mail.ru>
2023-06-15 19:53:09 +1100

    return comparison instead of if/else

---
+++ JS Basics/80.js
@@ -0,0 +1,22 @@
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
+  
+  
+  
+  /*function isEqual(a, b) {
+    if (a === b) {
+      return true;
+    } else {
+      return false;
+    }
+  } */
+  
+  
+  /*function isEqual(a, b) {
+    return a === b;
+  }*/
