9bb03bcd4278add1d0c0c3c0850b7b761dd6e577
Maksim Dorogoi <monarch2013@mail.ru>
2023-06-15 20:57:42 +1100

    Object intro

---
+++ JS Basics/83.js
@@ -0,0 +1,12 @@
+const myDog = {
+    // Only change code below this line
+  "name": "Rex",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["I", "NotACat"]
+  
+    // Only change code above this line
+  };
+  
+  
+  /*if your object has any non-string properties, JavaScript will automatically typecast them as strings; see also "Set/Map;WeakSet/WeakMap"*/
