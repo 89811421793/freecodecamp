3217c14dfbda6144b228fa253ad35f53e4c8d81d
Maksim Dorogoi <monarch2013@mail.ru>
2023-06-16 23:01:52 +1100

    Count Backwards With a For Loop

---
+++ JS Basics/99.js
@@ -0,0 +1,30 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i>=1; i -=2){
+  myArray.push(i);
+}
+
+
+
+/*
+for (let i = 9; i>=1; --i){          //i--
+  if(i%2==0) continue;
+  myArray.push(i);
+}
+*/
+
+
+/*
+for (let i = 9; i>=1; i--){          
+  if(i% 2 !== 0) myArray.push(i);
+}
+*/
+
+
+/*
+for (let i = 9; i>0; i -=1){          
+  if(i% 2) myArray.push(i);
+}*/
+
