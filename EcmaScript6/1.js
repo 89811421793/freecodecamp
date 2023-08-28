e02ffd9d40445f6578b53c8d625f31855ae57d26
Maksim Dorogoi <monarch2013@mail.ru>
2023-08-28 21:55:34 +1100

    var and let difference illustrated

---
+++ EcmaScript6/1.js
@@ -0,0 +1,11 @@
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+  
+  /* see also learn.javascript.ru for the details concerning var/let/const*/
