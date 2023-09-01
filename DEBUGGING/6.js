4a69fa7e70153b1760be3f26fecfa7038698a1d3
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-01 21:59:26 +1100

    backslash/different style of quotes

---
+++ DEBUGGING/6.js
@@ -0,0 +1,9 @@
+// using backslash(should you use one style of quotes)
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
+
+
+// or use different types of quotes
+/* let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+ console.log(innerHtml);
+ */
