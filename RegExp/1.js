7f379ce37a45dc5a3db8e38909ea685c060424f3
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-02 22:31:03 +1100

    .test() Method/regexes intro

---
+++ RegExp/1.js
@@ -0,0 +1,5 @@
+/*Regular expressions are used in programming languages to match parts of strings.*/
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); 
