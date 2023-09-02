742b8607d433aba2995eaea05a8a8a90a3d6a611
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-02 23:02:52 +1100

    .match()  method

---
+++ RegExp/5.js
@@ -0,0 +1,5 @@
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; 
+let result = extractStr.match(/coding/); // Change this line
+
+/*.match syntax is the "opposite" of the .test method*/
