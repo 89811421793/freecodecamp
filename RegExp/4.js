dc9c42bebdd517a7e0bb9d9bb278b144e5d4babe
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-02 22:49:22 +1100

    Ignore Case While Matching

---
+++ RegExp/4.js
@@ -0,0 +1,3 @@
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // flag 'i' will ignore the case (both lowercase and uppercase or the combo of two)
+let result = fccRegex.test(myString);
