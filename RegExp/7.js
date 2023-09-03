672f163ffc4a2912647b33641f28380fcc838e05
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-03 23:20:27 +1100

    Match Anything with Wildcard character(dot/period)

---
+++ RegExp/7.js
@@ -0,0 +1,5 @@
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+
+/*if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./*/
