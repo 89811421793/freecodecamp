a9fd94759e8937dfcf4a9f3942a078260a461a65
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-03 23:35:45 +1100

    character classes/[ ]

---
+++ RegExp/8.js
@@ -0,0 +1,7 @@
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+
+/*to match bag, big, and bug but not bog. You can create the regex /b[aiu]g/ to do this. The [aiu] is the character class that will only match the characters a, i, or u.*/
+
+/*Character classes allow you to define a group of characters you wish to match by placing them inside square ([ and ]) brackets*/
