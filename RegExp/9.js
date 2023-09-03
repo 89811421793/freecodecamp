693137f130c996612240b27171027d8bd4587ba8
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-03 23:41:28 +1100

    matching a large range of characters [ - ]

---
+++ RegExp/9.js
@@ -0,0 +1,5 @@
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+/*For example, to match lowercase letters a through e you would use [a-e]*/
