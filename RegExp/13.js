03692bb2bc50f1e8249d2cbae5eb364e47de7121
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-05 20:54:33 +1100

    matches characters that occur zero or more times.

---
+++ RegExp/13.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+let chewieRegex = /Aaaaaaaaaaaaaaaa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+
+/*For example:
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+// soccerWord.match(goRegex);
+// gPhrase.match(goRegex);
+// oPhrase.match(goRegex);
+
+// In order, the three match calls would return the values ["goooooooo"], ["g"], and null
