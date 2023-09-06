176ed677854be801bc0b116d8e2f602cbe7e84ad
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-06 22:50:14 +1100

    Mixed Grouping of Characters ()

---
+++ RegExp/30.js
@@ -0,0 +1,9 @@
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin D.|Eleanor)\sRoosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+
+/*to check for groups of characters through a Regular Expression we use parentheses ()*/
+
+
+/*If you want to find either Penguin or Pumpkin in a string, you can use the following Regular Expression: /P(engu|umpk)in/g*/
