094c85dd7d49e2c3aa4e77adcc591573629c4fd7
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-06 22:20:21 +1100

    Specify Exact Number of Matches

---
+++ RegExp/27.js
@@ -0,0 +1,5 @@
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/; // Change this line
+let result = timRegex.test(timStr);
+
+/*For example, to match only the word hah with the letter a 3 times, your regex would be /ha{3}h/ */
