433cde9fcb041dedcab21a14c57e038e0148e714
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-09 22:31:28 +1100

    method slice() to copy

---
+++ BASIC DATA STRUCTURES/7.js
@@ -0,0 +1,10 @@
+function forecast(arr) {
+    // Only change code below this line
+  let res = arr.slice(2, 4);
+    return res;
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+  
+  // 2 parameters — the first is the index at which to begin extraction, and the second is the index at which to stop extraction
