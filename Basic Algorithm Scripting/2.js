0d0a770dd9c0d130cba3bca1e92811f05baffa0a
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-13 22:10:18 +1100

    Reverse a String

---
+++ Basic Algorithm Scripting/2.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+    // let letters = str.split('');
+    // let backwards = letters.reverse();
+    // let joined = backwards.join('')
+    // return joined;
+  
+  return str.split('').reverse().join('');
+  
+  }
+  
+  reverseString("hello");
