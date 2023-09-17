430761c0747da52e345f2dc61e4aa00f7417ab93
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-17 23:35:08 +1100

    obj method

---
+++ Object Oriented Programming/3.js
@@ -0,0 +1,12 @@
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs() {
+      return 'This dog has 4 legs.'
+    }
+    //or: sayLegs: function() {
+    //   return 'This dog has 4 legs.'
+    // }
+  };
+  
+  dog.sayLegs();
