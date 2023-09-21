c5e9bcd9495fec75f9d20a47e16da2523a8198e2
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-21 21:19:08 +1100

    Functional Programming Intro

---
+++ FUNCTIONAL PROGRAMMING/1.js
@@ -0,0 +1,19 @@
+/*Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+// Only change code above this line
+
+/*Functional programming is a style of programming where solutions are simple, isolated functions, without any side effects outside of the function scope: INPUT -> PROCESS -> OUTPUT*/
