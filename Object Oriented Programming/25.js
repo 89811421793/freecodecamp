9638b309d1e9c457385625587937a0bc4cc8a7da
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-21 20:53:23 +1100

    IIFE (Immediately Invoked Function Expression)

---
+++ Object Oriented Programming/25.js
@@ -0,0 +1,8 @@
+(function () {
+    console.log("A cozy nest is ready");
+  })();
+  
+  /*Immediately Invoked Function Expression (IIFE): the syntax means that the func is created and executed right away simultaneously; this is an anonymous function expression;
+  the function has no name and is not stored in a variable. 
+  In the modern code it's used rarely.
+  */
