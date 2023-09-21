84dc7d2a6bb83e14476984fcda35a86647afdeb4
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-21 22:13:51 +1100

    avoiding mutations and side effects

---
+++ FUNCTIONAL PROGRAMMING/4.js
@@ -0,0 +1,10 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+return fixedValue+1
+  // Only change code above this line
+}
+
+/*One of the core principles of functional programming is to not change/alter things. Changes (mutations) lead to side effects and bugs. */
