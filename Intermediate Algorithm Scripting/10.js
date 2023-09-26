74babce286ff0357dd1a0d1f76638fcb74f03c5f
Maksim Dorogoi <monarch2013@mail.ru>
2023-09-26 21:27:00 +1100

    Sorted Union

---
+++ Intermediate Algorithm Scripting/10.js
@@ -0,0 +1,55 @@
+function uniteUnique(arr1, arr2, arr3) {
+    /*
+    // Creates an empty array to store our final result.
+    const finalArray = [];
+  
+    // Loop through the arguments object to truly make the program work with two or more arrays
+    // instead of 3.
+    for (let i = 0; i < arguments.length; i++) {
+      const arrayArguments = arguments[i];
+  
+      // Loops through the array at hand
+      for (let j = 0; j < arrayArguments.length; j++) {
+        let indexValue = arrayArguments[j];
+  
+        // Checks if the value is already on the final array.
+        if (finalArray.indexOf(indexValue) < 0) {
+          finalArray.push(indexValue);
+        }
+      }
+    }
+  
+    return finalArray;
+    */
+  
+    const args = [...arguments];
+    const result = [];
+    for (let i = 0; i < args.length; i++) {
+      for (let j = 0; j < args[i].length; j++) {
+        if (!result.includes(args[i][j])) {
+          result.push(args[i][j]);
+        }
+      }
+    }
+    return result;
+  }
+  
+  
+  /*rewritten function:
+  function uniteUnique(...arr) {
+    return [...new Set(arr.flat())];
+  }
+  
+  // Or as an arrow function
+  const uniteUnique = (...arr) => [...new Set(arr.flat())];
+  */
+  
+  /*Or:
+  function uniteUnique() {
+    return [...arguments]
+      .flat()
+      .filter((item, ind, arr) => arr.indexOf(item) === ind);
+  }
+  */
+  
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
