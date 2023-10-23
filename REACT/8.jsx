2fa21a140b7c1470430dad9db3b7e8b968804d90
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-23 22:28:27 +1100

    Create a Component (Class Syntax)

---
+++ REACT/8.jsx
@@ -0,0 +1,17 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      // Change code below this line
+   return(
+     <div>
+     <h1>Hello React!</h1>
+     </div>
+   ) 
+  
+      // Change code above this line
+    }
+  };
+  
+  {/*The class syntax of creating a component apparently uses the render() method*/}
