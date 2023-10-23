9a9f35298209683150ef631defeaf5814854f467
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-23 21:48:53 +1100

    React/JSX syntax intro

---
+++ REACT/1.jsx
@@ -0,0 +1,6 @@
+const JSX = <h1>Hello JSX!</h1>;
+
+// JSX is a combo of JS and html
+// React uses snapshots of its own DOM (virtual DOM) to optimize updating only specific parts of the actual DOM
+
+// ReactDOM.render(JSX, document.getElementById('root')). This function call is what places your JSX into React's own lightweight representation of the DOM
