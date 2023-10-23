c1ec46ba3670c76aac69fb7ea7341d677cb114fa
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-23 22:21:25 +1100

    Create a Component (Functional Way)

---
+++ REACT/7.jsx
@@ -0,0 +1,11 @@
+const MyComponent = function() {
+    // Change code below this line
+   return (
+      <div className=''>A Functional Component</div>
+    );
+  
+  
+    // Change code above this line
+  }
+  
+  {/*To create a component with a function(a Stateless Functional Component), you simply write a JavaScript function that returns either JSX or null; React, however, requires the function name to begin with a capital letter*/}
