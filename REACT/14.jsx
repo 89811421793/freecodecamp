ab8fc29e1273217b065f2220ec29977d28cdcf72
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-24 16:41:03 +1100

    props in React

---
+++ REACT/14.jsx
@@ -0,0 +1,51 @@
+// const CurrentDate = (props) => {
+//   return (
+//     <div>
+//       { /* Change code below this line */ }
+//       <p>The current date is: {props.date} </p>
+//       { /* Change code above this line */ }
+//     </div>
+//   );
+// };
+
+// class Calendar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h3>What date is it?</h3>
+//         { /* Change code below this line */ }
+//         <CurrentDate date = {Date()}/>
+//         { /* Change code above this line */ }
+//       </div>
+//     );
+//   }
+// };
+
+const CurrentDate = ({date}) => {
+    return (
+      <div>
+        { /* Change code below this line */ }
+        <p>The current date is: {date} </p>
+        { /* Change code above this line */ }
+      </div>
+    );
+  };
+  
+  class Calendar extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h3>What date is it?</h3>
+          { /* Change code below this line */ }
+          <CurrentDate date = {Date()} />
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
