03e5d88e6e36aa4d85c4c4400c604541cbe2f9eb
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-27 17:54:46 +1100

    Optimize Re-Renders with shouldComponentUpdate

---
+++ REACT/35.jsx
@@ -0,0 +1,48 @@
+{/*So far, if any component receives new state or new props, it re-renders itself and all its children*/}
+
+{/*React provides a lifecycle method you can call when child components receive new state or props, and declare specifically if the components should update or not. The method is shouldComponentUpdate(), and it takes nextProps and nextState as parameters.*/}
+
+class OnlyEvens extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+      console.log('Should I update?');
+      // Change code below this line
+      if (nextProps.value % 2 == 0) {
+          return true;
+        }
+        return false;
+     
+      // Change code above this line
+    }
+    componentDidUpdate() {
+      console.log('Component re-rendered.');
+    }
+    render() {
+      return <h1>{this.props.value}</h1>;
+    }
+  }
+  
+  class Controller extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 0
+      };
+      this.addValue = this.addValue.bind(this);
+    }
+    addValue() {
+      this.setState(state => ({
+        value: state.value + 1
+      }));
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.addValue}>Add</button>
+          <OnlyEvens value={this.state.value} />
+        </div>
+      );
+    }
+  }
