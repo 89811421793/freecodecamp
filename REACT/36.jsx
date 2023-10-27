9d1074a402fd2e1b53c1cd8df7909fe5e21004df
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-27 18:06:58 +1100

    inline styles in React

---
+++ REACT/36.jsx
@@ -0,0 +1,9 @@
+class Colorful extends React.Component {
+    render() {
+      return (
+        <div style={{color: "red", fontSize: '72px'}}>Big Red</div>
+      );
+    }
+  };
+  
+  {/*'72px' can be conveyed as 72 as well (px units are omitted*/}
