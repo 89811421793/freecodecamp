3c60b42ee05e0ca5e8adf3e1ba9fba09fd1878b5
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-28 22:32:50 +1100

    Define a Redux Action

---
+++ REDUX/3.js
@@ -0,0 +1,6 @@
+/*Updating state is one of Redux's core tasks. In Redux, all state updates are triggered by dispatching actions. An action is simply a JavaScript object that contains information about an action event that has occurred. The Redux store receives these action objects, then updates its state accordingly. */
+
+// Define an action here:
+const action = {
+    type: 'LOGIN'
+  }
