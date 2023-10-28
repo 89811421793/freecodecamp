14e7a65977450f4d516105fa82800b6c6f8fe831
Maksim Dorogoi <monarch2013@mail.ru>
2023-10-28 22:26:26 +1100

    Create a Redux Store

---
+++ REDUX/1.js
@@ -0,0 +1,13 @@
+/*Redux is a state management framework that can be used with a number of different web technologies, including React.
+
+In Redux, there is a single state object that's responsible for the entire state of your application. This means if you had a React app with ten components, and each component had its own local state, the entire state of your app would be defined by a single state object housed in the Redux store (the Redux store is an object which holds and manages application state)*/
+
+/* The reducer function takes state as an argument and returns state*/
+const reducer = (state = 5) => {
+    return state;
+  }
+  
+  // Redux methods are available from a Redux object
+  // For example: Redux.createStore()
+  // Define the store here:
+  const store = Redux.createStore(reducer)
